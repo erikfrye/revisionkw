1e41bf1649a79206f8e25d30021356b61d6dcca2
Erik Frye <Fireside@Eriks-MacBook-Air.local>
2019-12-04 19:23:20 -0500

    added GTM container to page

--- config/site.config.js
+++ config/site.config.js
@@ -9,13 +9,13 @@ if (!ROOT) {
 
 const config = {
   // Your website's name, used for favicon meta tags
-  site_name: '(re)vision',
+  site_name: 'Static Site Boilerplate',
 
   // Your website's description, used for favicon meta tags
-  site_description: '(re)vision home organizing Kitchener-Waterloo',
+  site_description: 'A modern boilerplate for static website development',
 
   // Your website's URL, used for sitemap
-  site_url: 'https://revisionkw.com',
+  site_url: '',
 
   // Google Analytics tracking ID (leave blank to disable)
   googleAnalyticsUA: '',
